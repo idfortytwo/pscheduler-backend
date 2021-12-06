ab92e146d3b4e07bca029bd7507f5ac062ff4443
Artem Buhera <artem.bugera@gmail.com>
2021-12-06 23:47:02 +0100

    showing execution logs - missing files

--- pscheduler/api/routers/__init__.py
+++ pscheduler/api/routers/__init__.py
@@ -1,2 +1,6 @@
-from api.routers import task_executor_router, task_router
+from api.routers import (
+    task_executor_router,
+    task_router,
+    execution_log_router
+)
 from api.routers._shared import router
