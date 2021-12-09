a64c29a6c16fdaefddc51ad1970cd27bc5d7161d
Artem Buhera <artem.bugera@gmail.com>
2021-12-09 22:05:26 +0100

    unsubscribing from output logs on execution finish

--- pscheduler/api/routers/execution_output_log_router.py
+++ pscheduler/api/routers/execution_output_log_router.py
@@ -4,7 +4,7 @@ from sqlalchemy import select
 
 from api.routers._shared import router
 from db.connection import Session
-from db.models import ExecutionOutputLog
+from db.models import ExecutionOutputLog, ExecutionLog
 
 
 @router.get('/execution/output/{execution_log_id}', status_code=200)
@@ -19,11 +19,18 @@ async def get_execution_output_log(execution_log_id: int, last_execution_output_
         if execution_output_logs:
             last_execution_output_log_id = execution_output_logs[-1].execution_output_log_id
 
+        status = None
+        if not execution_output_logs:
+            exec_log_stmt = select(ExecutionLog).filter(ExecutionLog.execution_log_id == execution_log_id)
+            execution_log: ExecutionLog = await session.scalar(exec_log_stmt)
+            status = execution_log.status
+
         return {
             'execution_output_logs': [
                 log.to_dict()
                 for log
                 in execution_output_logs
             ],
-            'last_execution_output_log_id': last_execution_output_log_id
+            'last_execution_output_log_id': last_execution_output_log_id,
+            'status': status
         }
