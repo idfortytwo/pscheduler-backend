5b7cc07d289e8959575b46775f2504b711dd7e2f
Artem Buhera <artem.bugera@gmail.com>
2021-12-10 20:17:35 +0100

    removed unused method

--- pscheduler/api/routers/task_executor_router.py
+++ pscheduler/api/routers/task_executor_router.py
@@ -10,15 +10,6 @@ async def get_executors():
     ]}
 
 
-@router.get('/executor/{task_id}', status_code=200)
-async def get_executor(task_id: int):
-    executor = task_manager.task_executors.get(task_id)
-    if executor:
-        return {'task_executor': executor.to_dict()}
-    else:
-        raise TaskNotFound(task_id)
-
-
 @router.post('/run_executor/{task_id}', status_code=200)
 async def run_executor(task_id: int):
     try:
