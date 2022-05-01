3e2c292ec794d9c8fc0db5f943a051cbbb98a53b
Artem Buhera <artem.bugera@gmail.com>
2022-05-01 16:05:30 +0200

    handling reading configuration from .env

--- pscheduler/db/connection.py
+++ pscheduler/db/connection.py
@@ -12,13 +12,19 @@ def sessionmaker(bind, class_: T) -> T:
     return sqlalchemy_sessionmaker(bind, class_)
 
 
-conn_config = dotenv_values('.env')
-user = conn_config.get('DB_USER')
-pwd = conn_config.get('DB_PASS')
-host = conn_config.get('DB_HOST')
-database = conn_config.get('DB_DATABASE')
-conn_str = f"postgresql+asyncpg://{user}:{pwd}@{host}/{database}"
-engine = create_async_engine(conn_str)
+try:
+    conn_config = dotenv_values('.env')
+    user = conn_config['DB_USER']
+    pwd = conn_config['DB_PASS']
+    host = conn_config['DB_HOST']
+    port = conn_config['DB_PORT']
+    database = conn_config['DB_DATABASE']
+    conn_str = f"postgresql+asyncpg://{user}:{pwd}@{host}:{port}/{database}"
+
+    engine = create_async_engine(conn_str)
+except KeyError as e:
+    print(f'Missing key {e.args[0]} in connection config')
+    exit(1)
 
 
 def Session(*args, **kwargs):
