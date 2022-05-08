a3a379c67efee13dd15a5cb7eb98a3bc0908a5f0
Artem Buhera <artem.bugera@gmail.com>
2022-05-08 13:06:11 +0200

    added metadata creation on start

--- pscheduler/main.py
+++ pscheduler/main.py
@@ -2,9 +2,14 @@ import asyncio
 
 from api.app import server
 from scheduler.executor import ExecutionManager
+from db.connection import engine
+from db.models import Base
 
 
 async def main():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
     async_task_manager = ExecutionManager()
     await async_task_manager.sync()
     async_task_manager.run_all()
